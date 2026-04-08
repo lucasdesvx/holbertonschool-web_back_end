23e9fc65e93223a9ad4bd4c73cc30f3590010dc5
Your Name <you@example.com>
2026-04-08 11:01:39 +0400

    m

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,4 +1,12 @@
- #!/usr/bin/env
+#!/usr/bin/env python3
+"""
+Module pour la fonction concat.
+"""
+
 
 def concat(str1: str, str2: str) -> str:
+    """
+    Concatène deux chaînes de caractères.
+    """
     return str1 + str2
+
