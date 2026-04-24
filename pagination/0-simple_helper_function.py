345eca0f4e4d790acd5d6e1aff08fef55fa1936d
Your Name <you@example.com>
2026-04-24 10:21:32 +0400

    m

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = page * page_size
