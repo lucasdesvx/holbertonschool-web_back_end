7f3fb5c15a272cdb37eae0ed0d8d8905cb2f6a29
Your Name <you@example.com>
2026-04-24 10:23:43 +0400

    m

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        indices = index_range(page, page_size)
+        data = self.dataset()
+
+        try:
+            return data[indices[0]:indices[1]]
+        except IndexError:
+            return []
