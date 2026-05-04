49919bb491d9c1d2d6c433feeb19e834a3b884e3
Your Name <you@example.com>
2026-05-04 10:49:09 +0400

    m

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+// 0-classroom.js
+
+export default class ClassRoom {
+  /**
+   * Represents a classroom.
+   * @param {Number} maxStudentsSize - The maximum number of students allowed.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
---
+++ ES6_classes/README.md
@@ -0,0 +1,1 @@
+if you didnt read the manga steel ball run, so you have to try it
