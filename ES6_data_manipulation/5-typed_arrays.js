31fa583bbde8f49eabfef7d447239f80701e944f
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-08-10 09:35:52 +0400

    m

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,8 @@
+/**
+ * Updates the grades of students from a specific city.
+ * @param {Array} students - The list of students.
+ * @param {String} city - The city to filter by.
+ * @param {Array} newGrades - The array of new grade objects.
+ * @returns {Array} A new array of students with updated grades.
+ */
+export default function  
