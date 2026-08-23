5883aab5ef4f040cd4c952f90e01139ff1842832
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-08-23 19:51:41 +0400

    m

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // You can perform asynchronous operations here.
+    // For this basic task, returning a new Promise is enough to pass the check.
+  });
+}
