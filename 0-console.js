1a6fd4e0992ff7a9a0a3cb3aa44d7d8620bcf24c
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-08 09:56:52 +0400

    m

---
+++ 0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message in STDOUT.
+ * @param {string} message - The string to print.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
