99b589dd6fd59aaa66cd502d8dfff753ca128b69
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 19:31:28 +0400

    m

--- ES6_basic/eslint.config.js
+++ ES6_basic/eslint.config.js
@@ -1,11 +1,9 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 module.exports = [
-  ...compat.config({
-    extends: ['./.eslintrc.js'],
-  }),
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
 ];
