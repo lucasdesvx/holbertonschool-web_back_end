0e6d42d32aad71b1ea494994bcae30c83b0c3d7b
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 19:30:00 +0400

    m

---
+++ ES6_basic/eslint.config.js
@@ -0,0 +1,11 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.config({
+    extends: ['./.eslintrc.js'],
+  }),
+];
