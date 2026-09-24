d3b6c3a91f1e5f158b8248e34b5c6f05b0ab0f55
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 19:56:35 +0400

    m

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-export function readDatabase(filePath) {
+function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (error, data) => {
       if (error) {
@@ -23,3 +23,5 @@ export function readDatabase(filePath) {
     });
   });
 }
+
+export default readDatabase;
