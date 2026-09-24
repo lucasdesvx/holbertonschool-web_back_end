a96034d4ed935e0f72ee2b3b8713c59c0e5c36f0
LucasDesvx <lucasdesvaux2008@gmail.com>
2026-09-24 19:13:19 +0400

    m

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -28,17 +28,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Returns the appropriate page of the dataset.
-
-        Args:
-            page (int): The 1-indexed page number.
-            page_size (int): The number of items per page.
-
-        Returns:
-            List[List]: A list of rows representing the requested page.
-        """
-        assert isinstance(page, int) and type(page) is int and page > 0
-        assert isinstance(page_size, int) and type(page_size) is int and page_size > 0
+        """Returns the appropriate page of the dataset."""
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
         start_idx, end_idx = index_range(page, page_size)
         data = self.dataset()
